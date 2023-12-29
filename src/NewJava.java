da6d05a87a3b30ff55410282f3b4d90beb1fd0cb
Mostafa Fata <mostafa.fata@gmail.com>
2023-12-28 21:20:27 -0800

    Update NewJava.java

--- src/NewJava.java
+++ src/NewJava.java
@@ -1,5 +1,6 @@
 public class NewJava {
     public static void main(String[] args) {
         System.out.println("Hello again!");
+        System.out.println("This line of code has been added from Github repo");
     }
 }
